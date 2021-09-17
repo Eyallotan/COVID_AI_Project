4761f01ead1d41fe9f95c9defe8c734133822431
tomerhaber18 <tomer.haber@campus.technion.ac.il>
2021-09-17 20:09:00 +0300

    extract new_cases columns

--- Preprocess/extract_daily_new_cases.py
+++ Preprocess/extract_daily_new_cases.py
@@ -1,5 +1,13 @@
 import pandas as pd
 from datetime import datetime
+'''
+right now every first row (first day) on each city has wrong value because of the shifts.
+this bug is irellevant if we dont need the first few days - so i think its ok
+if we want to calculate the first few days good, need to work on each city seperately or fix each first row with special
+treatment.
+for each row(for city_code in cities_df['City_Code'].unique())    
+'''
+
 
 def before_date():
     march20 = datetime(2020, 3, 28)
@@ -18,8 +26,6 @@ if __name__ == "__main__":
         cities_df[column_name] = pd.to_numeric(cities_df[column_name])
 
     # Generate N columns of previous days new cases
-    # for city_code in cities_df['City_Code'].unique():
-    #TODO: need to do this to every city seperately because of the shifts
     N = 8
     for i in range(1, N):
         cities_df[f'tmp_{i}'] = cities_df['Cumulative_verified_cases'].shift(periods=i)
@@ -28,12 +34,23 @@ if __name__ == "__main__":
         else:
             cities_df[f'verified_cases_{i}_days_ago'] = cities_df[f'tmp_{i-1}'] - cities_df[f'tmp_{i}']
 
-    # tmp code to test new columns
-    march20 = datetime(2020, 3, 28)
-    tmp_df = cities_df[(cities_df['Date'] < march20) & (cities_df['City_Code'] == 4000)]
+    for i in range(1, N):
+        cities_df.drop([f'tmp_{i}'], axis=1, inplace=True)
+    cities_df.rename(columns={'verified_cases_1_days_ago': 'today_verified_cases'}, inplace=True)
 
-    tmp_df = tmp_df[['Cumulative_verified_cases', 'verified_cases_1_days_ago', 'verified_cases_2_days_ago',
-                     'verified_cases_3_days_ago', 'verified_cases_4_days_ago', 'verified_cases_5_days_ago',
-                     'verified_cases_6_days_ago', 'verified_cases_7_days_ago']]
+    # tmp code to test new columns
+    start_date = datetime(2020, 7, 20)
+    end_date = datetime(2020, 11, 20)
+    interesting_columns = ['Cumulative_verified_cases', 'today_verified_cases', 'verified_cases_2_days_ago',
+                         'verified_cases_3_days_ago', 'verified_cases_4_days_ago', 'verified_cases_5_days_ago',
+                         'verified_cases_6_days_ago', 'verified_cases_7_days_ago']
+
+    # we work in specified date range
+    result_df = cities_df[(cities_df['Date'] > start_date) & (cities_df['Date'] < end_date)]
+
+    tlv_df = result_df[result_df['City_Code'] == 5000][interesting_columns]
+
+    march_20 = datetime(2020, 3, 28)
+    haifa_df = cities_df[(cities_df['Date'] < march_20) & (cities_df['City_Code'] == 4000)]
 
     print('~~~~')

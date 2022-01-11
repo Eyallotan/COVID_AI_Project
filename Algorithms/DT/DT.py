779c0772d7316230011d1d921c86d467af49a656
Dor Sura <dorsura@campus.technion.ac.il>
2022-01-12 00:12:56 +0200

    added documentation to DT.py
    Signed-off-by: Dor Sura <dorsura@campus.technion.ac.il>

--- Algorithms/DT/DT.py
+++ Algorithms/DT/DT.py
@@ -3,9 +3,14 @@ from Algorithms.DT import FeaturesSelection
 from Algorithms.DT import BestModel
 
 if __name__ == "__main__":
-
+    ################################################### PART A ############################################################
     DTvsRFComparison.run_DTvsRFComparison(city_code_flag="With City_Code", city_code_comparison_flag=False)
     print("====================================================================================")
+
+    '''
+    Sections B and C may take several hours to run and print their results 
+    '''
+    ################################################### PART B ############################################################
     FeaturesSelection.run_FeaturesSelection(pruning_flag="Without Pruning", avg_features_flag="Without Avg Features",
                                             only_features_importances_graph_flag=True)
     DTvsRFComparison.run_DTvsRFComparison(city_code_flag="With City_Code", city_code_comparison_flag=True)
@@ -17,7 +22,11 @@ if __name__ == "__main__":
     FeaturesSelection.run_FeaturesSelection(pruning_flag="With Pruning", avg_features_flag="Without Avg Features",
                                             only_features_importances_graph_flag=False)
     print("====================================================================================")
+
+    ################################################### PART C ############################################################
     FeaturesSelection.run_FeaturesSelection(pruning_flag="With Pruning", avg_features_flag="With Avg Features",
                                             only_features_importances_graph_flag=False)
     print("====================================================================================")
+
+    ################################################### SUMMARY ###########################################################
     BestModel.run_BestModel()
